7f57a453f1af7aaa8f60db6a3f85d461d8b8dbf8
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-12 13:02:21 +0100

    card component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,13 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import Food from "./Food"
+import Card from './Card.jsx';
 
 
 function App() {
   return(
     <>
-    <Header></Header>
-    <Food></Food>
-    <Footer></Footer>
+      
+      <Card title="Raouf Hamaissia" description="I am learning react" />
+      
+      
     </>
   );
 }
