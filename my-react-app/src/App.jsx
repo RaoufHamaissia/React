864ef53a63fc95f5f2435466f851cb615e8f4d5e
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-13 21:40:18 +0100

    Componant Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,17 @@
-import Button from './Button/Button.jsx';
+import Student from "./Student.jsx";
 
 
 function App() {
   return(
     <>
       
-      <Button/>
+      <Student name="Bob" age={30} isStudent={true}/>
+      <Student name="Alice" age={25} isStudent={false}/>
+      <Student name="Charlie" age={22} isStudent={true}/>
+      <Student name="David" age={28} isStudent={false}/>
+      <Student name="Eve" age={26} isStudent={true}/>
+      <Student />
+      
       
       
     </>
