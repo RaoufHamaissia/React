ad141cabec0882435e24c15244e752df4f4cee66
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-16 12:17:37 +0100

    rendering lists

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,24 @@
-import UserGreeting from "./UserGreeting.jsx";  
+import List from './List.jsx'
 
 
 function App() {
+  const fruits = [{id:1, name:'Apple', calories:95},
+                  {id:2, name:'Banana', calories:120},
+                  {id:3, name:'Cherry', calories:105},
+                  {id:4, name:'Date', calories:75},
+                  {id:5, name:'Elderberry', calories:130}];
+
+  const vegetables = [{id:6, name:'Carrot', calories:25},
+                      {id:7, name:'Broccoli', calories:55},
+                      {id:8, name:'Spinach', calories:23},
+                      {id:9, name:'Potato', calories:77},
+                      {id:10, name:'Cucumber', calories:16}];                
+                  
   return(
     <>
       
-     <UserGreeting isLoggedIn={true} username = "bob"  />
-     <br></br>
-     
+     {fruits.length > 0 && <List  category = "Fruits"/> }
+     {vegetables.length > 0 ? <List items = {vegetables} category = "Vegetables"/> : null}
       
       
       
