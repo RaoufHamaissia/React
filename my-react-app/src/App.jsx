576855c3c9194cb4662eaa1d2bec78d87e7f3f99
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-19 22:30:12 +0100

    useState() hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,22 @@
-import Button from './Button.jsx'
-import Picture from './Picture.jsx'
+// React hook = Special function that lets you use state and other React features without writing a class
+//
+//
+
+// useState() = React hook that lets you add state to a functional component 
+// useEffect() = React hook that lets you perform side effects in a functional component
+// useContext() = React hook that lets you use context in a functional component
+// useReducer() = React hook that lets you manage complex state in a functional component
+// useRef() = React hook that lets you create a mutable ref object in a functional component
+// useMemo() = React hook that lets you memoize a value in a functional component
+
+
+import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
+
+
+
+
+
 
 function App() {
                
@@ -7,7 +24,7 @@ function App() {
   return(
     <>
       
-     <Picture/>
+     <Counter/>
       
       
       
