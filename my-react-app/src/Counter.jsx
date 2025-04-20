9c2e61b89afdc68a5edae5f0ba838cf9dc427d45
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-20 22:11:18 +0100

    updater function

--- my-react-app/src/Counter.jsx
+++ my-react-app/src/Counter.jsx
@@ -5,18 +5,21 @@
         const [count, setCount] = useState(0);
 
         const increment = () => {
-            setCount(count + 1);
+            setCount(c => c + 1); // using functional update to avoid stale state
+            // setCount(count + 1); // this will work but it is not recommended as it can lead to stale state issues
+            
         }
 
         const decrement = () => {
             if (count === 0) {
                 return;
             }
-            setCount(count - 1);
+            setCount(c => c - 1); // using functional update to avoid stale state
+            // setCount(count - 1); // this will work but it is not recommended as it can lead to stale state issues
         }
 
         const reset = () => {
-            setCount(0);
+            setCount(0); 
         }
         return (
             <div className='counter-container'>
