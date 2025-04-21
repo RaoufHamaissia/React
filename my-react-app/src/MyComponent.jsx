a29c0c7dcb5abb09d8de38627c9b664789774e93
raouf hamaissia <r.hamaissia@gmail.com>
2025-04-21 22:33:09 +0100

    updating an array in a state

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,3 +1,4 @@
+import { element } from 'prop-types';
 import React,{useState} from 'react';
 // import { useState } from 'react';
     
@@ -7,26 +8,50 @@ import React,{useState} from 'react';
     
     function MyComponent() {
 
-        const [car, setCar] = useState({year: 2024, make: "Ford", model: "Mustang"});
-
-        const handelYearChange = (event) => {
-            setCar(c => ({...c, year: event.target.value}));
-        }
-        const handelMakeChange = (event) => {
-            setCar(c => ({...c, make: event.target.value}));
-        }
-        const handelModelChange = (event) => {
-            setCar(c => ({...c, model: event.target.value}));
-        }
+        const [foods, setFoods] = useState(["Apple", "Banana", "Orange"]);
         
+
+        const handelAddFood = () => {
+            const newFood = document.getElementById("foodInput").value;
+            document.getElementById("foodInput").value = "";
+
+            setFoods(f => [...f, newFood]);
+            // setFoods([...foods, newFood]);
+            // setFoods((prevFoods) => [...prevFoods, newFood]);
+        }
+
+        /*
+        const handelRemoveFood = () => {
+            const foodToRemove = document.getElementById("foodInput").value;
+            document.getElementById("foodInput").value = "";
+            setFoods(f => f.filter(food => food !== foodToRemove));
+            // setFoods(foods.filter(food => food !== foodToRemove));
+        }
+        */
+
+        const handelRemoveFood = (index) => {
+
+                setFoods(f => f.filter((_, i) => i !== index));
+                // _ is a placeholder for the first argument of the filter function, which is the current element. and is not used in this case.
+                // setFoods(foods.filter((element, i) => i !== index));
+        }
+
         return(
             
             <div>
-                <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
-
-                <input type="number" value={car.year} onChange={handelYearChange}/><br/>
-                <input type="text" value={car.make} onChange={handelMakeChange}/><br/>
-                <input type="text" value={car.model} onChange={handelModelChange}/><br/>
+                <h2>List of Food</h2>
+                <ul>
+                    {foods.map((food, index) => 
+                        <li key={index} onClick={() => handelRemoveFood(index)}>
+                            {food}
+                        </li>)}
+                        
+                            
+                </ul>
+                <input type="text" id="foodInput" placeholder='Enter food name' /><br/>
+                
+                <button onClick={handelAddFood}>Add Food</button>
+                <button onClick={handelRemoveFood}>Remove Food</button>
             </div>
 
 
